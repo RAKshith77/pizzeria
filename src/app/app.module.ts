b7477c013bda9130ca0b31cc8823d5aa7f6b739c
A06438_P5.Training <A06438_P5.Training@accenture.com>
2018-10-31 09:28:13 +0530

    stage1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrderPizzaComponent } from './order-pizza/order-pizza.component';
+import { BuildPizzaComponent } from './build-pizza/build-pizza.component';
+import { RouterModule } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrderPizzaComponent,
+    BuildPizzaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      [
+        {path:'',component:HomeComponent},
+        {path:'home',component:HomeComponent},
+        {path:'orderPizza',component:OrderPizzaComponent},
+        {path:'buildPizza',component:BuildPizzaComponent}
+
+      ]
+    )
+
   ],
   providers: [],
   bootstrap: [AppComponent]
